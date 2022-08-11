8486db4391a678a05fb78f102276bafc5eca2399
Shinjinwoo <jinu712@gmail.com>
2022-08-11 14:14:18 +0900

    상수정리

--- mediaplayer-object/src/main/java/com/tobesoft/plugin/mediaplayerobject/plugininterface/Define.java
+++ mediaplayer-object/src/main/java/com/tobesoft/plugin/mediaplayerobject/plugininterface/Define.java
@@ -7,23 +7,23 @@ public interface Define {
 
 
 
-    interface REQUEST_PERMISSION {
-
-        int CAMERA = 101;
-        int LOCATION = 102;
-        int STORAGE = 103;
-        int IGNORE_BATTERY = 104;
-        int LOCATION_ONE = 105;
-//        int CALL_PHONE = 106;
-        int CAMERA_GALLERY = 107;
-
-        int APP_STORAGE_ACCESS_REQUEST_CODE = 9301;
-    }
-
-    interface requestCode {
-        int SELECT_RINGTONE = 201;
-        int CAMERA_GALLERY = 202;
-    }
+//    interface REQUEST_PERMISSION {
+//
+//        int CAMERA = 101;
+//        int LOCATION = 102;
+//        int STORAGE = 103;
+//        int IGNORE_BATTERY = 104;
+//        int LOCATION_ONE = 105;
+////        int CALL_PHONE = 106;
+//        int CAMERA_GALLERY = 107;
+//
+//        int APP_STORAGE_ACCESS_REQUEST_CODE = 9301;
+//    }
+//
+//    interface requestCode {
+//        int SELECT_RINGTONE = 201;
+//        int CAMERA_GALLERY = 202;
+//    }
 
     interface ConstString {
         String PARAM_MEDIA_RESOURCE_TYPE = "mediaTypeFile";
